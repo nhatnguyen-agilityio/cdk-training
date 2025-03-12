875e8ce41084ca034c8f18aa3c38c6d33ba260c1
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-03-12 15:29:58 +0700

    feat(): create new stack and deploy it to aws

--- hello-cdk/hello_cdk/hello_cdk_stack.py
+++ hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
+    CfnOutput,
     Stack,
+    aws_lambda as _lambda
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -10,10 +12,27 @@ class HelloCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the Lambda function resource
+        my_function = _lambda.Function(
+            self, "HelloWorldFunction",
+            runtime = _lambda.Runtime.NODEJS_20_X,
+            handler="index.handler",
+            code=_lambda.Code.from_inline(
+                """
+                    exports.handler = async (event) => {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify('Hello World!')
+                        };
+                    };                                  
+                """
+            ),
+        )
+        
+        # Define the Lambda function uRL resource
+        my_function_url = my_function.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE
+        )
+        
+        # Define a CloudFormation output for your URL
+        CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
