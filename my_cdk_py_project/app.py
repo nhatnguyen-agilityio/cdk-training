2dc4ebc21ed3573171c59e53251d185ef4599c64
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-03-14 09:43:36 +0700

    feat(): create example for stage and deploy to aws

--- my_cdk_py_project/app.py
+++ my_cdk_py_project/app.py
@@ -4,25 +4,29 @@ import os
 import aws_cdk as cdk
 
 from my_cdk_py_project.my_cdk_py_project_stack import MyCdkPyProjectStack
+from my_cdk_py_project.my_stage import MyStage
 
 
 app = cdk.App()
-MyCdkPyProjectStack(app, "MyCdkPyProjectStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+# MyCdkPyProjectStack(app, "MyCdkPyProjectStack",
+#     # If you don't specify 'env', this stack will be environment-agnostic.
+#     # Account/Region-dependent features and context lookups will not work,
+#     # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+#     # Uncomment the next line to specialize this stack for the AWS Account
+#     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+#     #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+#     # Uncomment the next line if you know exactly what Account and Region you
+#     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+#     #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+#     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+#     )
+
+MyStage(app, "Dev", env=cdk.Environment(account="194722436838", region="us-east-1"))
+MyStage(app, "Prod", env=cdk.Environment(account="194722436838", region="us-east-1"))
 
 app.synth()
