5ee94dc3fc7b6ac8f3537a9420fc8fcb80287630
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-03-20 11:04:16 +0700

    feat(): create the first pipeline

--- my_pipeline_project/my_pipeline_project/my_pipeline_project_stack.py
+++ my_pipeline_project/my_pipeline_project/my_pipeline_project_stack.py
@@ -1,19 +1,28 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+import aws_cdk as cdk
 from constructs import Construct
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 
-class MyPipelineProjectStack(Stack):
+
+class MyPipelineProjectStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyPipelineProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        pipeline = CodePipeline(
+            self,
+            "Pipeline",
+            pipeline_name="MyPipeline",
+            synth=ShellStep(
+                "Synth",
+                input=CodePipelineSource.git_hub(
+                    "nhatnguyen-agilityio/cdk-training", "dev"
+                ),
+                commands=[
+                    "npm install -g aws-cdk",
+                    "python -m pip install -r requirements.txt",
+                    "cdk synth",
+                ],
+            ),
+        )
