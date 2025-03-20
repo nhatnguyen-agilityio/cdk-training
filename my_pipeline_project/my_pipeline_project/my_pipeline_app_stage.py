76e9e3749cd954d4135a4e51132fcd2239948f0b
Nhat Nnguyen <nhat.nguyen@asnet.com.vn>
2025-03-20 15:29:06 +0700

    feat(): update the import path

--- my_pipeline_project/my_pipeline_project/my_pipeline_app_stage.py
+++ my_pipeline_project/my_pipeline_project/my_pipeline_app_stage.py
@@ -1,6 +1,6 @@
 import aws_cdk as cdk
 from constructs import Construct
-from my_pipeline_lambda_stack import MyLambdaStack
+from .my_pipeline_lambda_stack import MyLambdaStack
 
 class MyPipelineAppStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
